90ab1c7d8aa78df9acbaed049114347b5d71be9a
longlodw <longlodw@gmail.com>
2025-07-23 05:36:43 -0400

    fix init.lua conflict

--- init.lua
+++ init.lua
@@ -164,6 +164,10 @@ vim.opt.scrolloff = 10
 
 vim.o.hlsearch = true
 vim.o.guicursor = 'a:blinkon500-blinkoff500,n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50'
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
