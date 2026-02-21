786f7c758cde776c57dc4b7a770bcec781a71ab1
longlodw <longlodw@gmail.com>
2026-02-21 17:48:27 -0500

    enable git blame on current line

--- init.lua
+++ init.lua
@@ -291,6 +291,7 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
     },
   },
 
